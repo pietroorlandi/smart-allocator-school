cccc23e919a3fea4a1094d906ddb30d5694486f5
nbernardoni <n.bernardoni@proteoeng.com>
2025-01-26 12:03:11 +0100

    - Added "gui" folder with the corresponding "gui.py" file, which loads the Streamlit interface. - Added "calls.py" file, which will contain the various calls made from the interface.

---
+++ src/gui/app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+# application title
+st.title("Resource Allocator Interface")
+st.write("This app helps you to allocare resources in the best way to save money!")
+
+# lateral bar
+if "api_key" not in st.session_state:
+    st.session_state.api_key = None
+
+st.sidebar.title("Insert your API Key")
+st.session_state.api_key = st.sidebar.text_input(
+    "API Key",
+    type="password",  # Nasconde il testo inserito
+    placeholder="Insert your API Key"
+)
+
+# budget
+budget = st.number_input(
+    "Insert your budget",
+    min_value=0.0,
+    value=1000000.0,
+    step=1000.0,
+    format="%.2f"
+)
+
+# insert a state
+states = [
+    "Rwanda" # , "Belize"
+]
+
+selected_state = st.selectbox(
+    "Select a state",
+    options=states
+)
+
+
+# submit button
+if st.button("Submit"):
+    if st.session_state.api_key:
+        st.success("Request Sumbitted")
+        # Puoi aggiungere qui il codice per elaborare la tua API Key
+    else:
+        st.error("Request Error!")
