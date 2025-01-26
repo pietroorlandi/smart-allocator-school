a7cb6443d037e38f5cb1bb518f5ac93d31266b11
andrea-montanari <montanari.andrea98@gmail.com>
2025-01-26 12:53:23 +0100

    Add SchoolsExtractor class to get connected and unconnected schools, and remove UnconnectedSchoolsExtractor

---
+++ src/schools_extractor/schools_extractor.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+class SchoolsExtractor:
+    def __init__(self, path):
+        self.path = path
+        self.data = self._load_data(self.path)
+
+    def _load_data(self, path):
+        data = pd.read_csv(path)
+        return data
+    
+    def get_unconnected_schools(self, country=None):
+        output_columns = ['country', 'education_level', 'school_name', 'latitude', 'longitude']
+        if country is None:
+            return self.data[self.data['connectivity'] == 'No'].reset_index(drop=True)[output_columns]
+        else:
+            return self.data[(self.data['country'] == country) & (self.data['connectivity'] == 'No')].reset_index(drop=True)[output_columns]
+        
+    def get_connected_schools(self, country=None):
+        output_columns = ['country', 'education_level', 'school_name', 'latitude', 'longitude']
+        if country is None:
+            return self.data[self.data['connectivity'] == 'Yes'].reset_index(drop=True)[output_columns]
+        else:
+            return self.data[(self.data['country'] == country) & (self.data['connectivity'] == 'Yes')].reset_index(drop=True)[output_columns]
